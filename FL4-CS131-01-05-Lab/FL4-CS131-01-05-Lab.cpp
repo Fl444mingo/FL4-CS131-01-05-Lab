e167147aea8ea05af0c6f642821ae05f4c0746c0
Fl444mingo <thespectralpotato@gmail.com>
2025-01-17 19:48:13 -0800

    Add project files.

---
+++ FL4-CS131-01-05-Lab/FL4-CS131-01-05-Lab.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, x, b;
+    cout << "y = mx + b\n\n";
+    cout << "Enter a value for m: ";
+    cin >> m;
+    cout << "Enter a value for x: ";
+    cin >> x;
+    cout << "Enter a value for b: ";
+    cin >> b;
+    cout << "\n";
+    cout << "Numbers: " << m << x << b << "\n\n";
+    system("pause");
+    return 0;
+}
