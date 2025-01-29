bd3da81eb071f14f45d7a6daa2fa80204d07f6a3
FL444mingo <thespectralpotato@gmail.com>
2025-01-29 14:00:36 -0800

    Program finished and commented

--- FL4-CS131-01-05-Lab/FL4-CS131-01-05-Lab.cpp
+++ FL4-CS131-01-05-Lab/FL4-CS131-01-05-Lab.cpp
@@ -2,16 +2,25 @@
 using namespace std;
 int main()
 {
-    int m, x, b;
-    cout << "y = mx + b\n\n";
-    cout << "Enter a value for m: ";
+    // Define variables
+    int m, x, b, window;
+    // Instructions on how to use the program, with specific user input prompts
+    cout << "Input integers to create ordered pairs of a line y = mx + b\n\n";
+    cout << "Enter a value for slope: ";
     cin >> m;
-    cout << "Enter a value for x: ";
-    cin >> x;
-    cout << "Enter a value for b: ";
+    cout << "Enter a value for y-intercept: ";
     cin >> b;
+    // window and x are queried in the same message rather than separately because they're closely related
+    cout << "Separated by a space, enter a domain and a starting x-value: ";
+    cin >> window;
+    cin >> x;
     cout << "\n";
-    cout << "Numbers: " << m << x << b << "\n\n";
+    // Loop to output exactly "window" number of ordered pairs
+    for (window; window > 0 ; window--) {
+        cout << "(" << x << ", " << m * x + b << ")\n";
+        x++;
+    }
+    // Pause to keep the console clean
     system("pause");
     return 0;
 }
